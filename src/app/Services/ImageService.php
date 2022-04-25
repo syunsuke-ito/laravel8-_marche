63aace7fc5e9a604176cff791fcbf6969505f9c8
syun <svikeyk21@gmail.com>
2022-04-25 18:37:25 +0900

    複数画像登録

--- src/app/Services/ImageService.php
+++ src/app/Services/ImageService.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Services;
 
@@ -26,10 +28,15 @@ class ImageService
      */
     public static function upload($imageFile, $folderName)
     {
+        if (is_array($imageFile)) {
+            $file = $imageFile['image'];
+        } else {
+            $file = $imageFile;
+        }
         $fileName = uniqid(rand() . '_');
-        $extension = $imageFile->extension();
+        $extension = $file->extension();
         $fileNameToStore = $fileName . '.' . $extension;
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+        $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
         Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
 
         return $fileNameToStore;
