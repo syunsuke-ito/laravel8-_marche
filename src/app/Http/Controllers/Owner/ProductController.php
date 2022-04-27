f03007350506be4bbb73d4d8adfc7b3497c59148
syun <svikeyk21@gmail.com>
2022-04-27 11:22:11 +0900

    category

--- src/app/Http/Controllers/Owner/ProductController.php
+++ src/app/Http/Controllers/Owner/ProductController.php
@@ -7,8 +7,9 @@ namespace App\Http\Controllers\Owner;
 use App\Http\Controllers\Controller;
 use App\Models\Image;
 use App\Models\Product;
-use App\Models\SecondaryCategory;
+use App\Models\PrimaryCategory;
 use App\Models\Owner;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -57,7 +58,7 @@ class ProductController extends Controller
         // $products = Owner::findOrFail(Auth::id())->shop->product;
 
         $owner_info = Owner::with('shop.product.imageFirst')
-        ->where('id', Auth::id())->get();
+            ->where('id', Auth::id())->get();
 
         // dd($owner_info, $products);
 
@@ -78,7 +79,19 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())
+            ->select('id', 'name')
+            ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+            ->select('id', 'title', 'filename')
+            ->orderBy('updated_at', 'desc')
+            ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+            ->get();
+
+        return view('owner.products.create', compact('shops', 'images', 'categories'));
     }
 
     /**
